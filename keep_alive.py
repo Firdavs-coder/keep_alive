fa0b458a1c7ad2cc3fa3b8470cb699105e6bd503
Firdavs <84655445+Firdavs-coder@users.noreply.github.com>
2023-01-07 19:41:01 +0500

    Create keep_alive.py

---
+++ keep_alive.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from threading import Thread
+import os
+
+app = Flask("app")
+
+STATIC_DIR = os.path.abspath('static')
+
+
+@app.route('/')
+def main():
+  return "Working"
+
+
+def run():
+  app.run("0.0.0.0", port=8080)
+
+
+def keep_alive():
+  server = Thread(target=run)
+  server.start()
+
+
+if __name__ == "__main__":
+  keep_alive()
